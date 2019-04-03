6332f5997e76c13457ea262ee4748efba298313c
Megha Priya <36860246+Megha762@users.noreply.github.com>
2019-04-03 18:06:22 +0530

    application uploaded

---
+++ MemeApp/src/Compnents/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+export default class Header extends React.Component {
+    render () {
+        return (
+            <div>
+                <header>
+                    
+                    <h1>
+                        <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt=""/>  Meme Generator
+                    </h1>
+                </header>
+            </div>
+        )
+    }
+}
